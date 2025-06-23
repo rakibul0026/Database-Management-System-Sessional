c10fb75fa4f89c28a9b4804311d22e00ed028f1b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-06-23 15:30:59 +0600

    Update view.sql

--- View/problem1/view.sql
+++ View/problem1/view.sql
@@ -2,3 +2,22 @@
 Create VIEW student_view AS
 SELECT Roll,Name
 FROM student_details;
+
+
+-- Q2. Insert View
+INSERT INTO student_view(Roll,Name,Gender)
+  VALUES (108,'Hasina','male');
+
+-- Q3. Update View
+UPDATE student_view
+SET Name='Fahim'
+where Roll=106;
+
+
+-- Q4. Delete View
+DELETE FROM student_view WHERE Roll=108
+
+-- Q5. DROP View
+DROP VIEW student_view
+
+
